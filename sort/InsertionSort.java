646f64f7a901678dd9526bad358407385d624ea1
Martin Gubala <2888769g@student.gla.ac.uk>
2025-02-13 10:38:57 +0000

    insertion, slelection, shell, merge

---
+++ sort/InsertionSort.java
@@ -0,0 +1,15 @@
+package sort;
+public class InsertionSort{
+    public static void sort(int a[]){
+    int n = a.length;
+    for (int j = 1; j < n; j++){
+    int key = a[j];
+    int i = j-1;
+    while ((i >= 0) && (a[i] > key)){
+    a[i+1] = a[i];
+    i--;
+    }
+    a[i+1] = key;
+    }
+    }
+}        
