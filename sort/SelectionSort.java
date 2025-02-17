df5426be56862e8864187194ebb0293b5176a53b
“Martin <2888769g@student.gla.ac.uk>
2025-02-17 11:41:54 +0000

    bottomup quicksort started

--- sort/SelectionSort.java
+++ sort/SelectionSort.java
@@ -1,19 +1,18 @@
 package sort;
-public class SelectionSort{
-    public static void sort(int a[]){
-    int n = a.length;
-    for (int i = 0; i < n - 1; i++){
-    int index = i;
-    //find minimum
-    for (int j = i + 1; j < n; j++){
-    if (a[j] < a[index]){
-    index = j;
-    }
-    }
-    //swap
-    int min = a[index];
-    a[index] = a[i];
-    a[i] = min;}
+
+public class SelectionSort {
+
+    public static void sort(int a[]) {
+        int n = a.length;
+        for (int i = 0; i < n - 1; i++) {
+            int index = i;
+            //find minimum
+            for (int j = i + 1; j < n; j++) {
+                if (a[j] < a[index]) {
+                    index = j;
+                }
+            }
+            Swap.swap(a, i, i);
+        }
     }
 }
-
--- sort/ShellSort.java
+++ sort/ShellSort.java
@@ -1,25 +1,23 @@
 package sort;
-public class ShellSort{ 
- 
-    public static void sort(int a[]){ 
-      int n = a.length; 
-      int h = 1; 
-       
-      while (h < n/3) h = 3*h + 1; 
-   
-      while (h >= 1) { 
-        for (int i = h; i < n; i++){ 
-          for (int j = i; j >= h && a[j] < a[j-h]; j -= h){ 
-            swap(a, j, j-h); 
-          } 
-        } 
-        h /= 3; 
-      } 
+
+public class ShellSort {
+
+    public static void sort(int a[]) {
+        int n = a.length;
+        int h = 1;
+
+        while (h < n / 3) {
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < n; i++) {
+                for (int j = i; j >= h && a[j] < a[j - h]; j -= h) {
+                    Swap.swap(a, j, j - h);
+                }
+            }
+            h /= 3;
+        }
     }
-    
-    private static void swap(int[] a, int i, int j) {
-        int temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-    }
+
 }
